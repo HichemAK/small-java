20da582142df9303b0d6f95d9f8da92094c20bf4
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2019-12-21 12:31:47 +0100

    Implementing 'Info' class

---
+++ src/com/utils/Info.java
@@ -0,0 +1,9 @@
+package com.utils;
+
+public class Info {
+    public String name, type;
+    public Info(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+}
--- src/com/utils/MyVisitor.java
+++ src/com/utils/MyVisitor.java
@@ -3,7 +3,7 @@ package com.utils;
 import com.gen.Small_JavaBaseVisitor;
 import com.gen.Small_JavaParser;
 
-public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
+public class MyVisitor extends Small_JavaBaseVisitor<Info> {
     private SymbolTable ST = new SymbolTable();
     private String type = null;
     private boolean biblang_exist = false;
@@ -26,25 +26,25 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
         }
     }
 
-    @Override public T visitR(Small_JavaParser.RContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitR(Small_JavaParser.RContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitImport_bib(Small_JavaParser.Import_bibContext ctx) {
+    @Override public Info visitImport_bib(Small_JavaParser.Import_bibContext ctx) {
         return visitChildren(ctx);
     }
     
-    @Override public T visitClass_declare(Small_JavaParser.Class_declareContext ctx) {
+    @Override public Info visitClass_declare(Small_JavaParser.Class_declareContext ctx) {
         return visitChildren(ctx);
     }
     
-    @Override public T visitClass_content(Small_JavaParser.Class_contentContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitClass_content(Small_JavaParser.Class_contentContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitVars_declare(Small_JavaParser.Vars_declareContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitVars_declare(Small_JavaParser.Vars_declareContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitMain(Small_JavaParser.MainContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitMain(Small_JavaParser.MainContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitInstruction(Small_JavaParser.InstructionContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitInstruction(Small_JavaParser.InstructionContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitAssign(Small_JavaParser.AssignContext ctx) {
+    @Override public Info visitAssign(Small_JavaParser.AssignContext ctx) {
         if(!biblang_exist){
             System.err.println(ctx.stop.getLine()+ ":" + ctx.stop.getCharPositionInLine() +
                     " :: Cannot use assign ':=' without importing Small_Java.lang");
@@ -53,7 +53,7 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
         return visitChildren(ctx);
     }
     
-    @Override public T visitIf_cond(Small_JavaParser.If_condContext ctx) {
+    @Override public Info visitIf_cond(Small_JavaParser.If_condContext ctx) {
         if(!biblang_exist){
             System.err.println(ctx.stop.getLine()+ ":" + ctx.stop.getCharPositionInLine() +
                     " :: Cannot use assign IF condition without importing Small_Java.lang");
@@ -61,7 +61,7 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
         return visitChildren(ctx);
     }
     
-    @Override public T visitRead(Small_JavaParser.ReadContext ctx) {
+    @Override public Info visitRead(Small_JavaParser.ReadContext ctx) {
         if(!bibio_exist){
             System.err.println(ctx.stop.getLine()+ ":" + ctx.stop.getCharPositionInLine() +
                     " :: Cannot use function 'In_SJ' without importing Small_Java.io");
@@ -70,7 +70,7 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
         return visitChildren(ctx);
     }
     
-    @Override public T visitWrite(Small_JavaParser.WriteContext ctx) {
+    @Override public Info visitWrite(Small_JavaParser.WriteContext ctx) {
         if(!bibio_exist){
             System.err.println(ctx.stop.getLine()+ ":" + ctx.stop.getCharPositionInLine() +
                     " :: Cannot use function 'Out_SJ' without importing Small_Java.io");
@@ -78,9 +78,9 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
         return visitChildren(ctx);
     }
     
-    @Override public T visitExp(Small_JavaParser.ExpContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitExp(Small_JavaParser.ExpContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitFactor(Small_JavaParser.FactorContext ctx) {
+    @Override public Info visitFactor(Small_JavaParser.FactorContext ctx) {
         for(int i=1;i<ctx.v().size();i++){
             if(ctx.mul_div(i-1).DIV() != null && ctx.v(i).getText().equals("0")){
                 System.err.println(ctx.stop.getLine()+ ":" + ctx.stop.getCharPositionInLine() +
@@ -90,7 +90,7 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
         return visitChildren(ctx);
     }
 
-    @Override public T visitV(Small_JavaParser.VContext ctx) {
+    @Override public Info visitV(Small_JavaParser.VContext ctx) {
         if(ctx.IDF() != null){
             checkIfDeclared(ctx.IDF().getText(), ctx.stop.getLine(), ctx.stop.getCharPositionInLine());
         }
@@ -99,15 +99,15 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
 
 
 
-    @Override public T visitExp_b(Small_JavaParser.Exp_bContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitExp_b(Small_JavaParser.Exp_bContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitFactor_b(Small_JavaParser.Factor_bContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitFactor_b(Small_JavaParser.Factor_bContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitLiteral(Small_JavaParser.LiteralContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitLiteral(Small_JavaParser.LiteralContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitAtom(Small_JavaParser.AtomContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitAtom(Small_JavaParser.AtomContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitVar_declare(Small_JavaParser.Var_declareContext ctx) {
+    @Override public Info visitVar_declare(Small_JavaParser.Var_declareContext ctx) {
         type = ctx.type().getText();
         for(int i=0;i<ctx.IDF().size();i++){
             checkIdfLength(ctx.IDF(i).getText(), ctx.stop.getLine(), ctx.stop.getCharPositionInLine());
@@ -125,7 +125,7 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
     }
 
 
-    @Override public T visitBibs(Small_JavaParser.BibsContext ctx) {
+    @Override public Info visitBibs(Small_JavaParser.BibsContext ctx) {
         if(ctx.BIB_IO() != null){
             bibio_exist = true;
         }
@@ -135,13 +135,13 @@ public class MyVisitor<T> extends Small_JavaBaseVisitor<T> {
         return visitChildren(ctx);
     }
     
-    @Override public T visitType(Small_JavaParser.TypeContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitType(Small_JavaParser.TypeContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitModif(Small_JavaParser.ModifContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitModif(Small_JavaParser.ModifContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitFormat(Small_JavaParser.FormatContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitFormat(Small_JavaParser.FormatContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitString(Small_JavaParser.StringContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitString(Small_JavaParser.StringContext ctx) { return visitChildren(ctx); }
     
-    @Override public T visitOp_compare(Small_JavaParser.Op_compareContext ctx) { return visitChildren(ctx); }
+    @Override public Info visitOp_compare(Small_JavaParser.Op_compareContext ctx) { return visitChildren(ctx); }
 }
